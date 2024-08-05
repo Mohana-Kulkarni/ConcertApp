7e2c926cfa00c324847a959bc611abebc3bc8d7f
Mohana-Kulkarni <mpkulkarni117@gmail.com>
2024-08-05 18:44:31 +0530

    Connected with API

--- app/src/main/java/com/example/concertticketingapp/MainActivity.java
+++ app/src/main/java/com/example/concertticketingapp/MainActivity.java
@@ -1,35 +1,58 @@
 package com.example.concertticketingapp;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import com.example.concertticketingapp.adapter.EventAdapter;
+import com.example.concertticketingapp.adapter.PlaceGridViewAdapter;
+import com.example.concertticketingapp.integration.RetrofitClient;
 import com.example.concertticketingapp.model.Category;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.concertticketingapp.model.Event;
+import com.example.concertticketingapp.model.Place;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 
+import androidx.cardview.widget.CardView;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.concertticketingapp.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.PopupWindow;
 
-import java.util.Locale;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    ProgressDialog mProgressDialog;
+    String cityName;
+    GridView cityGrid;
+    CardView cityCard;
+    View cityPopup;
+    EventAdapter eventAdapter;
+    PopupWindow popupWindow;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+//        mProgressDialog = new ProgressDialog(this);
+//        mProgressDialog.setIndeterminate(true);
+//        mProgressDialog.setMessage("Loading...");
+//        mProgressDialog.show();
+
 
 
 //        setSupportActionBar(binding.toolbar);
@@ -53,13 +82,91 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
+        LayoutInflater inflater = getLayoutInflater();
+        cityPopup = inflater.inflate(R.layout.city_popup, null);
+        cityGrid = cityPopup.findViewById(R.id.city_grid);
+        recyclerView = findViewById(R.id.eventsRecycler);
         addCategoryView();
 
+//        displayCityList();
 
+        cityCard = findViewById(R.id.cities);
+        cityCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayCityList();
+                showPopup(view);
+            }
+        });
 
     }
 
-    public void addCategoryView() {
+    public void displayCityList() {
+
+        System.out.println("In display");
+        RetrofitClient.getRetrofitInstance().getAPI().getPlaces().enqueue(new Callback<List<Place>>() {
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                Log.e("api","onFailure: " + t.getLocalizedMessage());
+//                if (mProgressDialog.isShowing())
+//                    mProgressDialog.dismiss();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+//                if (mProgressDialog.isShowing()){
+//                    mProgressDialog.dismiss();
+//                }
+
+                List<Place> cities = (List<Place>) response.body();
+                System.out.println(cities);
+
+                PlaceGridViewAdapter adapter = new PlaceGridViewAdapter(MainActivity.this, cities, cityName -> {
+                    popupWindow.dismiss();
+                    fetchEventsByCity(cityName);
+                });
+                cityGrid.setAdapter(adapter);
+            }
+
+        });
+    }
+
+
+    private void showPopup(View view) {
+
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focus = true;
+
+        popupWindow = new PopupWindow(cityPopup, width, height, focus);
+        popupWindow.showAtLocation(view, Gravity.CENTER,0,0);
+
+    }
+
+    private void fetchEventsByCity(String cityName) {
+        System.out.println(cityName);
+            RetrofitClient.getRetrofitInstance().getAPI().getEventByCity(cityName).enqueue(new Callback<List<Event>>() {
+            @Override
+            public void onResponse(Call<List<Event>> call, Response<List<Event>> response) {
+                List<Event> events = response.body();
+                System.out.println(events);
+                eventAdapter = new EventAdapter(MainActivity.this, events);
+                recyclerView.setAdapter(eventAdapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+            }
+
+            @Override
+            public void onFailure(Call<List<Event>> call, Throwable t) {
+                Log.e("api", "onFailure: " + t.getLocalizedMessage());
+            }
+        });
+
+    }
+
+
+
+
+    private void addCategoryView() {
         HorizontalScrollView categoryScrollView = findViewById(R.id.category_scroll);
         LinearLayout categoryContainer = new LinearLayout(this);
         categoryContainer.setOrientation(LinearLayout.HORIZONTAL);
