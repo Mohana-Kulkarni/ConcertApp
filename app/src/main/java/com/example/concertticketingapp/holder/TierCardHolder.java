11475aeee030442095ac8e6a9b8b66ca6ab1425e
Mohana-Kulkarni <mpkulkarni117@gmail.com>
2024-09-04 22:57:27 +0530

    cardview for add,remove and count added in holder

--- app/src/main/java/com/example/concertticketingapp/holder/TierCardHolder.java
+++ app/src/main/java/com/example/concertticketingapp/holder/TierCardHolder.java
@@ -4,24 +4,35 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.concertticketingapp.R;
 
 public class TierCardHolder extends RecyclerView.ViewHolder {
 
-    TextView tierName, tierPrice, tierSeats;
+    TextView tierName, tierPrice, tierSeats, ticketCount;
+    public CardView addBtn;
+    public CardView removeBtn;
+    public int count = 0;
 
     public TierCardHolder(@NonNull View itemView) {
         super(itemView);
         tierName = itemView.findViewById(R.id.tier_name);
         tierSeats = itemView.findViewById(R.id.available_seats);
         tierPrice = itemView.findViewById(R.id.tier_price);
+        ticketCount = itemView.findViewById(R.id.ticket_count);
+        removeBtn = itemView.findViewById(R.id.remove);
+        addBtn = itemView.findViewById(R.id.add);
     }
 
     public void setTierData(String tierName, int tierPrice, int tierSeats) {
         this.tierName.setText(tierName);
         this.tierSeats.setText(tierSeats + " available");
         this.tierPrice.setText("Rs. " + tierPrice);
+    }
+
+    public void updateTicketCount() {
+        ticketCount.setText(String.valueOf(count));
     }
 }
